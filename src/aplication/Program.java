891b97d92c761393fe298cf37dfba16d16bf6224
Rafael Costa <raf.costa29@gmail.com>
2019-07-15 10:39:01 -0300

    Versao ruim

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -23,8 +23,7 @@ public class Program {
 
 		if (!checkOut.after(checkIn)) {
 			System.out.println("Error in reservation: Check-out must be after check-in date");
-		} 
-		else {
+		} else {
 			Reservation reservation = new Reservation(number, checkIn, checkOut);
 			System.out.println("Reservation: " + reservation);
 
@@ -35,16 +34,10 @@ public class Program {
 			System.out.print("Check-out date (dd/MM/yyyy): ");
 			checkOut = sdf.parse(sc.next());
 
-			Date now = new Date();
-			if (checkIn.before(now) || checkOut.before(now)) {
-				System.out.println("Error in reservation: Reservation dates for update must be future");
-			} 
-			else if (!checkOut.after(checkIn)) {
-				System.out.println("Error in reservation: Check-out must be after check-in date");
-			} 
-			else {
-
-				reservation.updateDates(checkIn, checkOut);
+			String error = reservation.updateDates(checkIn, checkOut);
+			if (error != null) {
+				System.out.println("Error in reservation: " + error);
+			} else {
 				System.out.println("Reservation: " + reservation);
 			}
 		}
